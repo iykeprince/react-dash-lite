80b9656898baf53bdc00e18057acb994d4a554f1
Piemonte Spectre <iykeprince48@gmail.com>
2021-06-21 00:57:36 +0100

    changes have been made to modify private route

--- src/utils/private.route.js
+++ src/utils/private.route.js
@@ -1,18 +1,23 @@
 import { Redirect, Route } from "react-router";
 
-const PrivateRoute = ({component: Component, ...rest}) => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
     const token = localStorage.getItem('BITFETTER_AUTH_TOKEN');
-
+    console.log('token', token)
+    if (!token) {
+        console.log('no token of you')
+    } else {
+        console.log('check we have token')
+    }
     return (
-        <Route 
-            {...rest} 
-            render={({location, ...otherProps}) => {
-                if(!token){
-                    window.location.href= '/login';
+        <Route
+            {...rest}
+            render={({ location, ...otherProps }) => {
+                if (!token) {
+                    window.location.href = '/login';
                     return true;
-                }else{
+                } else {
                     return <Component {...otherProps} />
-                }              
+                }
             }}
         />
     )
