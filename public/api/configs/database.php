0e11add0abecd371a8dc609fb26b5d50d7f375c7
Piemonte Spectre <iykeprince48@gmail.com>
2021-06-20 14:43:40 +0100

    updated clear db credentials

--- public/api/configs/database.php
+++ public/api/configs/database.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ * HEROKU CLEARDB DEPLOYMENT REQUIREMENT
+ */
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'bitfetter_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', $url["host"]);
+define('DB_NAME', substr($url["path"], 1));
+define('DB_USER', $url["user"]);
+define('DB_PASS', $url["pass"]);
+
+/**
+ * define('DB_TYPE', 'mysql');
+*define('DB_HOST', 'localhost');
+*define('DB_NAME', 'bitfetter_db');
+*define('DB_USER', 'root');
+*define('DB_PASS', '');
+ */
 
 /**
  * 
